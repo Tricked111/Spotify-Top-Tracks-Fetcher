4d7d8ad6705b674c836ac722fa00bcad3d837406
Tricked111 <shift3114@gmail.com>
2023-04-04 09:58:53 +0200

    fix

--- services/api/app.py
+++ services/api/app.py
@@ -9,41 +9,47 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Spotify API
-spot = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
-    client_id = os.getenv('SPOTIPY_CLIENT_ID'),
-    client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
-))
+spot = spotipy.Spotify(
+    auth_manager=SpotifyClientCredentials(
+        client_id=os.getenv("SPOTIPY_CLIENT_ID"),
+        client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
+    )
+)
 
 # Flask
 app = Flask(__name__)
 cors = CORS(app)
-app.config['JSON_SORT_KEYS'] = False
+app.config["JSON_SORT_KEYS"] = False
 
-@app.route('/api/v1/<string:search>',methods=['GET'])
+
+@app.route("/api/v1/<string:search>", methods=["GET"])
 def search(search):
     """
     This function will return the artist name, genres and top tracks
     """
-    result_serch = spot.search(q='artist:' + search,
-                          type='artist',
-                          limit=10)
+    result_serch = spot.search(q="artist:" + search, type="artist", limit=10)
 
     if result_serch:
-        artist = result_serch['artists']['items'][0]
-        tracks = spot.artist_top_tracks(artist['uri'])
-        
-        return jsonify({'artist_name': artist['name'],
-                        'genres': artist['genres'],
-                        'tracks' : [{
-                            'track_name': track['name'],
-                            'album_name': track['album']['name'],
-                            'popularity': track['popularity'],
-                            'img'       : track['album']['images'][0]['url']
-                            
-                        }for track in tracks['tracks']]
-                        })
-    return jsonify({'error': 'Artist not found'})
+        artist = result_serch["artists"]["items"][0]
+        tracks = spot.artist_top_tracks(artist["uri"])
 
-if __name__ == '__main__':
-    app.run(debug =True,host ='0.0.0.0')
-    
+        return jsonify(
+            {
+                "artist_name": artist["name"],
+                "genres": artist["genres"],
+                "tracks": [
+                    {
+                        "track_name": track["name"],
+                        "album_name": track["album"]["name"],
+                        "popularity": track["popularity"],
+                        "img": track["album"]["images"][0]["url"],
+                    }
+                    for track in tracks["tracks"]
+                ],
+            }
+        )
+    return jsonify({"error": "Artist not found"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
